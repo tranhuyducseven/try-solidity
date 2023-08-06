97a19242d8e4dc5a7eed7a6f2134b9ba9ece411f
tranhuyducseven <tranhuyducseven@users.noreply.github.com>
2023-08-06 12:57:07 +0700

    combie dna

--- factory.sol
+++ factory.sol
@@ -1,5 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED 
-pragma solidity >=0.8.0;
+pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
 
@@ -15,10 +14,13 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    // declare mappings here
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
 
-    function _createZombie(string memory _name, uint _dna) private {
+    function _createZombie(string memory _name, uint _dna) internal {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
     }
 
@@ -28,7 +30,9 @@ contract ZombieFactory {
     }
 
     function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
+        randDna = randDna - randDna % 100;
         _createZombie(_name, randDna);
     }
 
